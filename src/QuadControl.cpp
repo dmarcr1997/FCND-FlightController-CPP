75e579acd609d95ec61bb765bb68441db502283c
Damon Marc Rocha II <dmarcr1997@gmail.com>
2023-01-06 11:54:14 -0500

    Updated for test 5

--- src/QuadControl.cpp
+++ src/QuadControl.cpp
@@ -91,26 +91,33 @@ V3F QuadControl::RollPitchControl(V3F accelCmd, Quaternion<float> attitude, floa
 {
   V3F pqrCmd;
   Mat3x3F R = attitude.RotationMatrix_IwrtB();
-  float R_11 = R(0, 0);
-  float R_12 = R(0, 1);
-  float R_13 = R(0, 2);
-  float R_21 = R(1, 0);
-  float R_22 = R(1, 1);
-  float R_23 = R(1, 2);
-  float R_33 = R(2, 2);
 
-  float c = collThrustCmd / mass;
-  V3F b = V3F(R(0, 2), R(1, 2), 0.f);
-  V3F b_c = V3F(accelCmd.x / -c, accelCmd.y / -c, 0.f);
-  b_c.constrain(-maxTiltAngle, maxTiltAngle);
+  if (collThrustCmd > 0.f) {
+       float R_11 = R(0, 0);
+       float R_12 = R(0, 1);
+       float R_13 = R(0, 2);
+       float R_21 = R(1, 0);
+       float R_22 = R(1, 1);
+       float R_23 = R(1, 2);
+       float R_33 = R(2, 2);
 
-  V3F b_error = b_c - b;
-  V3F b_c_dot = kpBank * b_error;
+       float c = collThrustCmd / mass;
+       V3F b = V3F(R(0, 2), R(1, 2), 0.f);
+       V3F b_c = V3F(accelCmd.x / -c, accelCmd.y / -c, 0.f);
+       b_c.constrain(-maxTiltAngle, maxTiltAngle);
 
-  pqrCmd.x = (R_21 * b_c_dot.x - R_11 * b_c_dot.y) / R_33;
-  pqrCmd.y = (R_22 * b_c_dot.x - R_12 * b_c_dot.y) / R_33;
-  pqrCmd.z = 0.f;
+       V3F b_error = b_c - b;
+       V3F b_c_dot = kpBank * b_error;
 
+       pqrCmd.x = (R_21 * b_c_dot.x - R_11 * b_c_dot.y) / R_33;
+       pqrCmd.y = (R_22 * b_c_dot.x - R_12 * b_c_dot.y) / R_33;
+       pqrCmd.z = 0.f;
+  }
+  else {
+       pqrCmd.x = 0.f;
+       pqrCmd.y = 0.f;
+       pqrCmd.z = 0.f;
+  }
   return pqrCmd;
 }
 
@@ -142,14 +149,13 @@ V3F QuadControl::LateralPositionControl(V3F posCmd, V3F velCmd, V3F pos, V3F vel
   V3F accelCmd = accelCmdFF;
 
   velCmd = kpPosXY * (posCmd - pos);
-  velCmd.x = CONSTRAIN(velCmd.x, -maxSpeedXY, maxSpeedXY);
-  velCmd.y = CONSTRAIN(velCmd.y, -maxSpeedXY, maxSpeedXY);
+  velCmd.constrain(-maxSpeedXY, maxSpeedXY);
 
   const V3F err_dot = velCmd - vel;
   accelCmd += kpVelXY * err_dot;
 
-  accelCmd.x = CONSTRAIN(accelCmd.x, -maxAccelXY, maxAccelXY);
-  accelCmd.y = CONSTRAIN(accelCmd.y, -maxAccelXY, maxAccelXY);
+  accelCmd.constrain(-maxAccelXY, maxAccelXY);
+
 
   return accelCmd;
 }
